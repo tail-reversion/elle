a4cff4e372559eb47e67c2710115b80b5f3e47e4
Kelly Smith <kellysmith12.21@gmail.com>
2021-01-17 06:44:01 -0700

    remove thunk form

--- elle-lib/private/procedure.rkt
+++ elle-lib/private/procedure.rkt
@@ -1,4 +1,4 @@
 #lang elle/private/prebase
 
 {reprovide racket/base #:exposing procedure?  compose #:as ∘}
-{reprovide racket/function #:exposing identity const thunk curry}
+{reprovide racket/function #:exposing identity const curry}
