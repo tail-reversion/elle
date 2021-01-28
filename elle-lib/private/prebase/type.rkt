05a2540268b92e6bd165ef71b26c8e655a57d0dc
Kelly Smith <kellysmith12.21@gmail.com>
2021-01-27 21:38:52 -0700

    remove define-type

--- elle-lib/private/prebase/type.rkt
+++ elle-lib/private/prebase/type.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
-(provide define-type
-         (rename-out
+(provide (rename-out
           [@%define-singleton-type define-singleton-type]
           [@%define-wrapper-type define-wrapper-type]
           [@%define-object-type define-object-type]))
@@ -72,99 +71,3 @@
                                          (default-object-properties desc)))))])
 
 
-{begin-for-syntax
-  (define-record-type tuple-type-desc (name predicate-name fields))
-
-  (define-record-type record-type-desc (name predicate-name fields))
-
-  (define-wrapper-type enum-case)
-  
-  (define-record-type algebraic-type (name predicate tuple-bindings record-bindings enum-binding))
-  
-  (define-syntax-class type-case-declaration
-    #:description "type case"
-    #:attributes (type)
-    [pattern (name:id field:id ...+)
-             #:attr type (tuple-type-desc #:name #'name
-                                          #:predicate-name (format-id #'name "~a?" #'name)
-                                          #:fields (datum (field ...)))]
-    [pattern (name:id field:keyword ...+)
-             #:attr type (record-type-desc #:name #'name
-                                           #:predicate-name (format-id #'name "~a?" #'name)
-                                           #:fields (map (compose string->symbol keyword->string syntax-e)
-                                                         (datum (field ...))))]
-    [pattern name:id
-             #:attr type (enum-case #'name)])
-
-  (define-splicing-syntax-class type-case-declaration-set
-    #:description "type case declarations"
-    #:attributes (tuples records enum-cases)
-    [pattern {~seq tycase:type-case-declaration ...+}
-             #:attr tuples (filter tuple-type-desc? (datum (tycase.type ...)))
-             #:attr records (filter record-type-desc? (datum (tycase.type ...)))
-             #:attr enum-cases (map enum-case-value (filter enum-case? (datum (tycase.type ...))))])
-  }
-
-(define-syntax-parser define-type
-  #:track-literals
-  [(_ name:id
-      tycases:type-case-declaration-set
-      {~alt {~optional {~seq #:inspector inspector-expr}
-                       #:name "#:inspector option"}
-            {~seq #:property prop prop-val:expr}} ...)
-   #:declare inspector-expr (expr/c #'inspector?)
-   #:declare prop (expr/c #'struct-type-property?)
-   #:with inspector #'{~? inspector-expr.c (current-inspector)}
-   #:with pred-name (format-id #'name "~a?" #'name)
-   #:with enum-name (format-id #'name "~a:enum" #'name)
-   #:with enum-pred-name (format-id #'enum-name "~a?" #'enum-name)
-   #:with (tuple-names ...) (map tuple-type-desc-name (attribute tycases.tuples))
-   #:with (record-names ...) (map record-type-desc-name (attribute tycases.records))
-   #:with (pred-names ...) (cons #'enum-pred-name (append (map tuple-type-desc-predicate-name (attribute tycases.tuples))
-                                                          (map record-type-desc-predicate-name (attribute tycases.records))))
-   #:with inspector-name (generate-temporary 'inspector)
-   #:with (prop-val-name ...) (generate-temporaries (datum (prop ...)))
-   #:with prop-list-name (generate-temporary 'prop-list)
-   #`(begin (define inspector-name inspector)
-            (define prop-val-name prop-val) ...
-            (define prop-list-name (list (cons prop prop-val-name) ...))
-            (define-enum-type enum-name #,(attribute tycases.enum-cases)
-              #:predicate-name enum-pred-name
-              #:inspector inspector-name
-              #:property-maker (λ (desc)
-                                 (append prop-list-name
-                                         (default-enum-properties desc))))
-            (define (tuple-prop-maker desc)
-              (append prop-list-name
-                      (default-tuple-properties desc)))
-            #,@(for/list ([tuple-ty (attribute tycases.tuples)])
-                 #`(define-tuple-type #,(tuple-type-desc-name tuple-ty) #,(tuple-type-desc-fields tuple-ty)
-                     #:predicate-name #,(tuple-type-desc-predicate-name tuple-ty)
-                     #:inspector inspector-name
-                     #:property-maker tuple-prop-maker))
-            (define (record-prop-maker desc)
-              (append prop-list-name
-                      (default-record-properties desc)))
-            #,@(for/list ([record-ty (attribute tycases.records)])
-                 #`(define-record-type #,(record-type-desc-name record-ty) #,(record-type-desc-fields record-ty)
-                     #:predicate-name #,(record-type-desc-predicate-name record-ty)
-                     #:inspector inspector-name
-                     #:property-maker record-prop-maker))
-            (define pred-name (λ (x) (or (pred-names x) ...)))
-            (define-syntax name (algebraic-type #:name #'name
-                                                #:predicate #'pred-name
-                                                #:tuple-bindings (list #'tuple-names ...)
-                                                #:record-bindings (list #'record-names ...)
-                                                #:enum-binding #'enum-name)))])
-
-
-
-; test case
-#;(require racket/match)
-#;(define-type one-or-two
-    (one-proc val)
-    (two-procs val1 val2)
-    #:property prop:procedure (λ (self a b)
-                                (match self
-                                  [(one-proc f) (f a b)]
-                                  [(two-procs f g) (f (g a b))])))
